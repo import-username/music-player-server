6a1b1ace6f67d13b28cfba45b1a58d1c23076f7d
import-username <stermanv2@gmail.com>
2021-10-21 17:31:40 -0400

    main module now calls initializeRoutes function

--- dist/server/main.js
+++ dist/server/main.js
@@ -1,10 +1,12 @@
 "use strict";
 exports.__esModule = true;
 var express = require("express");
+var initializeRoutes_1 = require("./routes/initializeRoutes");
 var PORT = parseInt(process.env.PORT) || 5000;
 var app = express();
+(0, initializeRoutes_1["default"])(app);
 app.get("/", function (req, res) {
-    res.status(200).json({
+    return res.status(200).json({
         message: "response!"
     });
 });
--- src/server/main.ts
+++ src/server/main.ts
@@ -1,10 +1,13 @@
 import * as express from "express";
+import initializeRoutes from "./routes/initializeRoutes";
 
 const PORT: number = parseInt(process.env.PORT) || 5000;
 const app: express.Express = express();
 
-app.get("/", (req: express.Request, res: express.Response): void => {
-    res.status(200).json({
+initializeRoutes(app);
+
+app.get("/", (req: express.Request, res: express.Response): express.Response => {
+    return res.status(200).json({
         message: "response!"
     });
 });
