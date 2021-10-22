4ce6e23c11f411ad4b2201e4067b719aa967e841
import-username <stermanv2@gmail.com>
2021-10-21 20:30:34 -0400

    Added findByEmail function

--- dist/server/tables/users.js
+++ dist/server/tables/users.js
@@ -1,9 +1,43 @@
 "use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 exports.__esModule = true;
-exports.findById = void 0;
+exports.findByEmail = exports.findById = void 0;
 var databasePool_1 = require("../util/databasePool");
-function findById(email, password, callback) {
-    var queryText = "SELECT * FROM users WHERE email=$1;";
-    databasePool_1["default"].query(queryText, [email], callback);
+/**
+ * Queries postgresql database users table for a single row with id.
+ * @param {string} id Id to query for.
+ * @param callback
+ */
+function findById(id, callback) {
+    var queryText = "SELECT * FROM users WHERE id=$1;";
+    databasePool_1["default"].query(queryText, [id], function (err, queryResult) {
+        if (err) {
+            return callback(err, null);
+        }
+        return callback(null, __spreadArray([], queryResult.rows, true)[0]);
+    });
 }
 exports.findById = findById;
+/**
+ * Queries postgresql database users table for a single row with email.
+ * @param {string} email Email to query for.
+ * @param callback
+ */
+function findByEmail(email, callback) {
+    var queryText = "SELECT * FROM users WHERE email=$1;";
+    databasePool_1["default"].query(queryText, [email], function (err, queryResult) {
+        if (err) {
+            return callback(err, null);
+        }
+        return callback(null, __spreadArray([], queryResult.rows, true)[0]);
+    });
+}
+exports.findByEmail = findByEmail;
--- src/server/tables/users.ts
+++ src/server/tables/users.ts
@@ -1,7 +1,37 @@
+import { QueryResult } from "pg";
+import { UsersQueryCallback } from "../../ts/types/users";
 import dbPool from "../util/databasePool";
 
-export function findById(email: string, password: string, callback) {
-    const queryText: string = "SELECT * FROM users WHERE email=$1;";
+/**
+ * Queries postgresql database users table for a single row with id.
+ * @param {string} id Id to query for.
+ * @param callback 
+ */
+export function findById(id: string, callback: UsersQueryCallback): void {
+    const queryText: string = "SELECT * FROM users WHERE id=$1;";
 
-    dbPool.query(queryText, [email], callback);
+    dbPool.query(queryText, [id], (err: Error, queryResult: QueryResult) => {
+        if (err) {
+            return callback(err, null);
+        }
+
+        return callback(null, [...queryResult.rows][0]);
+    });
+}
+
+/**
+ * Queries postgresql database users table for a single row with email.
+ * @param {string} email Email to query for.
+ * @param callback 
+ */
+export function findByEmail(email: string, callback: UsersQueryCallback): void {
+    const queryText: string = "SELECT * FROM users WHERE email=$1;"
+
+    dbPool.query(queryText, [email], (err: Error, queryResult: QueryResult) => {
+        if (err) {
+            return callback(err, null);
+        }
+
+        return callback(null, [...queryResult.rows][0]);
+    });
 }
