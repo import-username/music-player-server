170794745d228aee20112d885361fbe6514616b2
import-username <stermanv2@gmail.com>
2021-11-04 20:58:26 -0400

    Added get-songs endpoint for getting user songs

--- dist/server/routes/song.js
+++ dist/server/routes/song.js
@@ -5,6 +5,8 @@ var auth_1 = require("../middleware/auth");
 var path = require("path");
 var fs = require("fs");
 var uploadFile_1 = require("../middleware/uploadFile");
+var songs_1 = require("../tables/songs");
+var createGetSongsQueryOptions_1 = require("../middleware/createGetSongsQueryOptions");
 var router = express.Router();
 var uploadPath = process.env.UPLOAD_DIR || path.join(__dirname, "..", "..", "uploads");
 if (!fs.existsSync(uploadPath)) {
@@ -13,6 +15,30 @@ if (!fs.existsSync(uploadPath)) {
 function songRoute() {
     router.post("/upload-song", auth_1["default"], uploadFile_1["default"], function (req, res) {
         return res.sendStatus(200);
+    });
+    router.get("/get-songs", auth_1["default"], createGetSongsQueryOptions_1["default"], function (req, res) {
+        songs_1["default"].find({ user_id: req.user.id }, req.queryOptions, function (err, result) {
+            if (err) {
+                return res.status(500).json({
+                    message: "Internal server error."
+                });
+            }
+            var response = {
+                tables: result.rows.map(function (row) {
+                    var rowObj = {};
+                    for (var i in row) {
+                        if (i !== "id" && i !== "user_id") {
+                            rowObj[i] = row[i];
+                        }
+                    }
+                    return rowObj;
+                })
+            };
+            if (result.total) {
+                response.total = result.total;
+            }
+            return res.status(200).json(response);
+        });
     });
     return router;
 }
--- src/server/routes/song.ts
+++ src/server/routes/song.ts
@@ -4,6 +4,10 @@ import * as Busboy from "busboy";
 import * as path from "path";
 import * as fs from "fs";
 import uploadFile from "../middleware/uploadFile";
+import Songs from "../tables/songs";
+import { IAuthRequest } from "../../ts/interfaces/authenticatedRequest";
+import createGetSongsQueryOptions from "../middleware/createGetSongsQueryOptions";
+import { IRelationRequest } from "../../ts/interfaces/relation";
 
 const router: express.Router = express.Router();
 
@@ -17,6 +21,36 @@ if (!fs.existsSync(uploadPath)) {
 export default function songRoute(): express.Router {
     router.post("/upload-song", auth, uploadFile, (req: express.Request, res: express.Response): void | express.Response => {
         return res.sendStatus(200);
+    });
+
+    router.get("/get-songs", auth, createGetSongsQueryOptions, (req: IRelationRequest, res: express.Response): void | express.Response => {
+        Songs.find({ user_id: req.user.id }, req.queryOptions, (err: Error, result: any) => {
+            if (err) {
+                return res.status(500).json({
+                    message: "Internal server error."
+                });
+            }
+
+            let response: any = {
+                tables: result.rows.map((row) => {
+                    let rowObj = {};
+        
+                    for (let i in row) {
+                        if (i !== "id" && i !== "user_id") {
+                            rowObj[i] = row[i];
+                        }
+                    }
+        
+                    return rowObj;
+                })
+            }
+            
+            if (result.total) {
+                response.total = result.total;
+            }
+
+            return res.status(200).json(response);
+        });
     });
 
     return router;
