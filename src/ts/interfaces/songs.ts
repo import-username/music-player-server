c4f8dd341a89120af4034f74f3068c27a4b196f9
import-username <stermanv2@gmail.com>
2021-11-16 21:32:01 -0500

    Added interfaces for songs route

--- src/ts/interfaces/songs.ts
+++ src/ts/interfaces/songs.ts
@@ -1,3 +1,5 @@
+import { IAuthRequest } from "./authenticatedRequest";
+
 export interface ISaveQuery {
     id: string,
     user_id: string,
@@ -16,4 +18,20 @@ export interface ISaveQueryOptionals {
     author?: string,
     favorite?: boolean | "TRUE" | "FALSE",
     playlists?: Array<String>
+}
+
+export interface ISongData {
+    id?: string,
+    user_id?: string,
+    song_file_path?: string,
+    song_thumbnail_path?: string,
+    song_title?: string,
+    song_description?: string,
+    song_author?: string,
+    song_favorite?: "TRUE" | "FALSE",
+    song_playlists?: string
+}
+
+export interface IUploadSongRequest extends IAuthRequest {
+    songData?: ISongData
 }
