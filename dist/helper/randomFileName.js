ca383279a1256026c368340c0410b11e8bc80b07
import-username <stermanv2@gmail.com>
2022-02-05 23:50:40 -0500

    removed dist directory

--- dist/helper/randomFileName.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function getRandomFileName() {
-    var filename = Date.now() + "-" + Math.floor(Math.random() * 10E9);
-    return filename;
-}
-exports["default"] = getRandomFileName;
